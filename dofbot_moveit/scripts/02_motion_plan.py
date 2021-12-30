9473a99a44d3c733319f6238cf4e67794fe46ed2
dharapatel1596 <dharapatel1596@gmail.com>
2021-12-30 22:16:01 +0800

    updated 02_motion file

--- dofbot_moveit/scripts/02_motion_plan.py
+++ dofbot_moveit/scripts/02_motion_plan.py
@@ -8,9 +8,24 @@ import moveit_commander
 from geometry_msgs.msg import Pose, PoseStamped
 from moveit_commander import MoveGroupCommander,PlanningSceneInterface
 from tf.transformations import quaternion_from_euler
+import sqlite3
+from sqlite3 import Error
 
 
 DE2RA = pi / 180
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return conn
 
 if __name__ == '__main__':
 
@@ -40,21 +55,33 @@ if __name__ == '__main__':
 
     dofbot.set_max_acceleration_scaling_factor(1.0)
     #brown spot
-    j1 = (88 - 90) * DE2RA
-    j2 = (54 - 90) * DE2RA
-    j3 = (51 - 90) * DE2RA
-    j4 = (13 - 90) * DE2RA
-    j5 = (90 - 90) * DE2RA
-    target_joints1 = [j1, j2, j3, j4, j5]
-    #red spot
-    j1 = (115 - 90) * DE2RA
-    j2 = (28 - 90) * DE2RA
-    j3 = (75 - 90) * DE2RA
-    j4 = (33 - 90) * DE2RA
-    j5 = (90 - 90) * DE2RA
-    target_joints2 = [j1, j2, j3, j4, j5]
-    #target_joints2 = [0.47124,-1.134464,-0.47888,-0.59341,0]
-    #target_joints2 = [90.0,53.0,33.0,36,270.0]
+    database = r"/home/jetson/dofbotDB"
+    conn = create_connection(database)
+    target_joints1 = [0.0, 0.0, 0.0, 0.0, 0.0]
+    target_joints2 = [0.0, 0.0, 0.0, 0.0, 0.0]
+    with conn:
+        print("connection received\n")
+	cur = conn.cursor()
+	cur.execute("select * from SpotPosition  where spotname='brown'")
+	raw = cur.fetchall()
+    
+        (joint1, joint2, joint3, joint4, joint5, spot)= tuple(raw[0])
+        target_joints1[0] = (joint1 - 90) * DE2RA
+        target_joints1[1] = (joint2 - 90) * DE2RA
+        target_joints1[2] = (joint3 - 90) * DE2RA
+        target_joints1[3] = (joint4 - 90) * DE2RA
+        target_joints1[4] = (joint5 - 90) * DE2RA
+        # for joint2
+        cur.execute("select * from SpotPosition  where spotname='red'")
+        raw = cur.fetchall()
+    
+        (joint1, joint2, joint3, joint4, joint5, spot) = tuple(raw[0])
+        target_joints2[0] = (joint1 - 90) * DE2RA
+        target_joints2[1] = (joint2 - 90) * DE2RA
+        target_joints2[2] = (joint3 - 90) * DE2RA
+        target_joints2[3] = (joint4 - 90) * DE2RA
+        target_joints2[4] = (joint5 - 90) * DE2RA
+    
     tool_size = [0.03,0.03,0.03]
     end_effector_link = dofbot.get_end_effector_link()
     
@@ -67,60 +94,25 @@ if __name__ == '__main__':
     p.pose.orientation.y = 0
     p.pose.orientation.z = 0
     p.pose.orientation.w = 1
-#    scene.attach_box(end_effector_link. 'tool',p,tool_size)
 
     while 1:
         dofbot.set_joint_value_target(target_joints1)
-        #Arm.Arm_serial_servo_write(6,135,400)
         dofbot.go()
-        sleep(2)
-        print("brown finish")
-        Arm.Arm_serial_servo_write(6,135,200)
-        print("grip sent")
-        sleep(2)
-        print("grip finish")
-        
-        dofbot.set_named_target("up")
+        sleep(0.5)
+        Arm.Arm_serial_servo_write(6,135,100)
+        sleep(0.5)
+
+        dofbot.set_named_target("top")
         dofbot.go()
-        sleep(2)
+        sleep(0.5)
 
         dofbot.set_joint_value_target(target_joints2)
         dofbot.go()
-        Arm.Arm_serial_servo_write(6,60,200)
-        sleep(2)
+        #sleep(0.5)
+        Arm.Arm_serial_servo_write(6,60,100)
+        #sleep(0.5)
 
-        dofbot.set_named_target("up")
+        dofbot.set_named_target("top")
         dofbot.go()
-        sleep(2)
-    #pos = Pose()
+        sleep(0.5)
 
-    #pos.position.x = 0.0
-    #pos.position.y = 0.0597016
-    #pos.position.z = 0.168051
-
-    #roll = -140.0
-    #pitch = 0.0
-    #yaw = 0.0
-
-    #q = quaternion_from_euler(roll * DE2RA, pitch * DE2RA, yaw * DE2RA)
-    # pos.orientation.x = 0.940132
-    # pos.orientation.y = -0.000217502
-    # pos.orientation.z = 0.000375234
-    # pos.orientation.w = -0.340811
-    #pos.orientation.x = q[0]
-    #pos.orientation.y = q[1]
-    #pos.orientation.z = q[2]
-    #pos.orientation.w = q[3]
-    
-    #dofbot.set_pose_target(pos)
-
-    #for i in range(5):
-
-    #    plan = dofbot.plan()
-    #    if len(plan.joint_trajectory.points) != 0:
-    #        print ("plan success")
-
-     #       dofbot.execute(plan)
-     #       break
-     #   else:
-     #       print ("plan error")
